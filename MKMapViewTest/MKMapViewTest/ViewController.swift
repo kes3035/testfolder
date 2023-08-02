6c7152e7eccd94dbbc791516f95937039567c121
kes3035 <kes3035@gmail.com>
2023-08-02 20:02:11 +0900

    MKMapView 테스트 프로젝트 생성

---
+++ MKMapViewTest/MKMapViewTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MKMapViewTest
+//
+//  Created by 김은상 on 2023/08/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
